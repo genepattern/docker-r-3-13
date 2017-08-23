7a71f89d23c9aa43ad696de5d0ad99793eccbb76
ted Liefeld <liefeld@broadinstitute.org>
2017-08-23 11:49:32 -0700

    changed test dir

--- tests/affy/runConfig.sh
+++ tests/affy/runConfig.sh
@@ -7,7 +7,7 @@ JOB_DEFINITION_NAME="R313_Generic"
 JOB_ID=gp_job_R313_$1
 JOB_QUEUE=TedTest
 S3_ROOT=s3://moduleiotest
-WORKING_DIR=$PWD/job_12345
+WORKING_DIR=$PWD/job_52345
 
 COMMAND_LINE="Rscript --no-save --quiet --slave --no-restore $TASKLIB/run_gp_affyst_efc.R $TASKLIB --input.file=$INPUT_FILE_DIRECTORIES/inputFileList.txt --normalize=yes --background.correct=no --qc.plot.format=skip --annotate.rows=yes --output.file.base=tedsOutputFile"
 
