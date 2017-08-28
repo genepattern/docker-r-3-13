480f70538cc85220fc591c2f66efabba04f10f46
ted Liefeld <liefeld@broadinstitute.org>
2017-08-28 14:56:21 -0700

    adding example time logging

--- runS3Batch_prerun_custom.sh
+++ runS3Batch_prerun_custom.sh
@@ -7,7 +7,18 @@ export R_LIBS=/usr/local/lib/R/site-library
 ##################################################
 # mount pre-compiled libs from S3
 echo "FOR R3. 13 CUSTOMIZING: aws s3 sync $S3_ROOT$R_LIBS_S3 $R_LIBS --quiet"
-aws s3 sync $S3_ROOT$R_LIBS_S3 $R_LIBS --quiet
+
+#TIMEFORMAT='It takes %R seconds to sync the RLIBs ..'
+#time {
+    #command block that takes time to complete...
+    #........
+    aws s3 sync $S3_ROOT$R_LIBS_S3 $R_LIBS --quiet
+#}
+
+echo RLIB size is
+du -sh $R_LIBS
+
+#aws s3 sync $S3_ROOT$R_LIBS_S3 $R_LIBS --quiet
 
 if [ -f "$TASKLIB/r.package.info" ]
 then
